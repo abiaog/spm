e4111831b1424071534b86d269c80346a2f48a49
abiaog <abiaog@126.com>
2021-11-22 20:27:37 +0800

    <auto push>

--- relaunch.sh
+++ relaunch.sh
@@ -3,6 +3,7 @@
 source kill_all_tj.sh
 cp ./.taskjugglerrc.example ./.taskjugglerrc 
 tj3d -c ./.taskjugglerrc -p 8474  --webserver --debug
-tj3client add spm.tjp
+# tj3client add spm.tjp
+tj3client add nr.tjp
 
 # visit in http://127.0.0.1:8080/ or in a remote server with port 8474
